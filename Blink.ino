a51a00315d68f66795fb0f20039f154c41180780
ReVolt-YanJ <93377958+ReVolt-YanJ@users.noreply.github.com>
2021-11-05 16:51:37 +0800

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -30,8 +30,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(6000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
   delay(6000);                       // wait for a second
 }
